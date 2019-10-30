0515e3278a20f5d5049c2dc7eae428c4d8ded620
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-30 09:46:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
       months = {:season => holiday_name}
     end
   end
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
